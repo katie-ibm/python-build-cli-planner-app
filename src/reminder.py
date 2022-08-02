45c7de6b748856dad79aaa90a077dddfcf9415f8
Katie Harshman <katie@ibm.com>
2022-08-02 08:16:34 -0700

    tasks 1 and 2

--- src/reminder.py
+++ src/reminder.py
@@ -1,3 +1,6 @@
+from sys import prefix
+
+
 class PrefixedReminder:
     """This class acts as a base class for other types of reminders.
     Classes that subclass it should override the `self.text` property
@@ -5,3 +8,11 @@ class PrefixedReminder:
     def __init__(self, prefix="Hey, don't forget to "):
         self.prefix = prefix
         self.text = prefix + '<placeholder_text>'
+
+class PoliteReminder(PrefixedReminder):
+    """This class inherits from PrefixReminder.
+    It will help make our text more exciting.
+    """
+    def __init__(self, text):
+        super().__init__("please ")
+        self.text = self.prefix + text
