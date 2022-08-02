582840782c816cd0ad0cd1f3b8301fc23bbde3f2
Katie Harshman <katie@ibm.com>
2022-08-02 08:56:28 -0700

    modules thru 7

---
+++ src/deadlined_reminders.py
@@ -0,0 +1,25 @@
+from abc import ABCMeta, abstractmethod, ABC
+from collections.abc import Iterable
+from dateutil.parser import parse
+from datetime import datetime
+
+class DeadlinedMetaReminder(Iterable, metaclass=ABCMeta):
+    @abstractmethod
+    def is_due(self):
+        pass
+
+class DeadlinedReminder(ABC, Iterable):
+    @abstractmethod
+    def is_due(self):
+        pass
+
+class DateReminder(DeadlinedReminder):
+    def __init__(self, text, date):
+        self.text = text
+        self.date = parse(date, dayfirst=True)
+    
+    def __iter__(self):
+        return iter([self.text, self.date.isoformat()])
+
+    def is_due(self):
+        return self.date <= datetime.now()
